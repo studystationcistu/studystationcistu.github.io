68fbdb381f94365e6e3681f9436be46983a5aa81
Nanon <nanonzazanano@gmail.com>
2026-04-17 15:59:42 +0700

    fix favicon

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -14,9 +14,6 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: 'Study Station CISTU',
   description: 'ยืมอุปกรณ์ห้องคอมมอน',
-  icons: {
-    icon: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxMDAgMTAwIj48cmVjdCB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgcng9IjIwIiBmaWxsPSIjZmVmMGYyIiAvPjx0ZXh0IHg9IjUwIiB5PSI2NSIgZm9udC1zaXplPSI1MCIgdGV4dC1hbmNob3I9Im1pZGRsZSI+8J+TpjwvdGV4dD48L3N2Zz4=",
-  }
 }
 
 export default function RootLayout({ children }) {
